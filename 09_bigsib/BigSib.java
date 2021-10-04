27cfa2efdcce7b2c14f9bc188560906cacbb9586
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-03 22:35:59 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -5,15 +5,25 @@
 *
 
 *DISCOVERIES
-*
+* In order to run one function in the other,
 *
 *Unresolved questions:
 *
 * 
 */
-public class BigSib {
 
-	public static void BigSib(String input){
+
+public class Bigsib {
+
+
+	public static void greet(String input){
 		System.out.println("How is it going, fellow thinker"+input);
-  }
+}
+
+	public static void main(String[] args){
+		greet(" Ameer! How is the weather holding up?");
+		greet(" Jacob! Hope you woke up on the right side of bed :D");
+		greet(" Josiah! How was your transport to school today?");
+}
+
 }
