bd2d1c8e52bb8c8beb2fbd72fbbba10190d4fc8d
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-03 22:48:55 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,17 +1,15 @@
-/**Ameer Alnasser
+/**Never Advise Children:: Ameer Alnasser-Turtle Boi, Corina Chen-Binktop, Nakib Abedin-Joker
+*Ameer Alnasser
 *APCS
 *HW09
 *2021-10-01
 *
-
 *DISCOVERIES
-* In order to run one function in the other,
-*
+*In order to run a function with another, they must be in the same directory
+* We learned how to run a function with another file 
 *Unresolved questions:
-*
-* 
+* N/A
 */
-
 
 public class Bigsib {
 
