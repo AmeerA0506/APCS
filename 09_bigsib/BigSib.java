fe3f2391569f45e011bd15f85b0d2320a2a16dad
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-04 22:22:55 -0400

    Add files via upload

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,15 +1,17 @@
-/**Never Advise Children:: Ameer Alnasser-Turtle Boi, Corina Chen-Binktop, Nakib Abedin-Joker
-*Ameer Alnasser
+/**Ameer Alnasser
 *APCS
 *HW09
 *2021-10-01
 *
+
 *DISCOVERIES
-*In order to run a function with another, they must be in the same directory
-* We learned how to run a function with another file 
+* In order to run one function in the other,
+*
 *Unresolved questions:
-* N/A
+*
+* 
 */
+
 
 public class BigSib {
 
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -13,8 +13,8 @@
 */
 public class Greet {
     public static void main( String[] args ) {
-        BigSib.greet("Flim");
-        BigSib.greet("Flam");
-        BigSib.greet("Kazaam");
+        BigSib.greet(" Flim!");
+        BigSib.greet(" Flam!");
+        BigSib.greet(" Kazaam!");
     }
 }
