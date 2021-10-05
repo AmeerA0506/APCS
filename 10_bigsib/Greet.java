59f51fde1bc3c0e9c01557717427609e4752e6d7
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-04 22:26:19 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/**Never Advise Children:: Ameer Alnasser-Turtle Boi, Corina Chen-Binktop, Nakib Abedin-Joker
+*Ameer Alnasser
+*APCS
+*HW10
+*2021-10-01
+*
+
+*DISCOVERIES
+*
+*Unresolved questions:
+* N/A
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
