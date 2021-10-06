291b38025e51dcbde2ef52f41fad199e3b52a9f2
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-05 22:04:35 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,24 @@
+/**Aardvarks Are Just Kidding:: Ameer Alnasser,Jacob Kirmayer
+*APCS
+*HW12
+*2021-10-05
+*
+*DISCO:
+*string must be initialized in both Greet and BigSib
+*Instance variables are called inside the class
+*QCC: 
+*What is the greet java file doing exactly? what is the point of richard?
+
+
+public class BigSib {
+    public String greeting;
+
+    public void setHelloMsg(String message){
+        greeting = message;
+    }
+
+    public String greet(String input){
+        return (greeting+" "+input);
+    }
+
+}
