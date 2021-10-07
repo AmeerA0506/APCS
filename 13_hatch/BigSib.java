452076730f491527af39e7e7f6d86fc564d8cfa5
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-07 00:10:08 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,25 +1,27 @@
 /*
-Aardvarks Are Just Kidding:: Ameer Alnasser, Jacob Kirmayer
-APCS
-HW13
-2021-10-06
-DISCO
-
-QCC 
-
+*Aardvarks Are Just Kidding:: Ameer Alnasser, Jacob Kirmayer
+*APCS
+*HW13
+*2021-10-06
+*DISCO
+*NEED TO REMEMBER WHERE I NEED TO CLOSE THE BRACKETS!!!
+*Constructors in the beginning allow for the "null" originally valued in greeting to Word up
+*QCC 
+*What is the issue with making greeting public or private? they both work code-wise
 */
 
 public class BigSib {
-    private HelloMsg{
-        String greeting;
-        greeting="Hello";
+    private String greeting;
+    public BigSib(){
+             greeting="Word up";
+	}
+    public String greet(String input){
+        return (greeting+" "+input);
+    }
 
     public void setHelloMsg(String message){
         greeting = message;
     }
 
-    public String greet(String input){
-        return (greeting+" "+input);
-    }
 
 }
