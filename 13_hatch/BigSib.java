457c884061e2de47f2c8cadc1ff193f531ab387c
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-06 23:53:01 -0400

    Create BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,25 @@
+/*
+Aardvarks Are Just Kidding:: Ameer Alnasser, Jacob Kirmayer
+APCS
+HW13
+2021-10-06
+DISCO
+
+QCC 
+
+*/
+
+public class BigSib {
+    private HelloMsg{
+        String greeting;
+        greeting="Hello";
+
+    public void setHelloMsg(String message){
+        greeting = message;
+    }
+
+    public String greet(String input){
+        return (greeting+" "+input);
+    }
+
+}
