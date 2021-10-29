996f74a826c4136de50e1529fbfaf38a81d39755
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-10-28 22:23:52 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,4 +1,17 @@
+/**
+Not Chewing Around :: Ameer Alnasser, Corina Chen, Nada Hameed (with the GUDFAMness of Sophia and Gabriel)
+APCS
+HW26::For Loops and string edits!!
+2021-10-28
+time taken:1.3hr
 
+DISCO:
+need to do String.length() to return the int value of length
+
+QCC:
+is there a shorter way to reverse a string?
+
+**/
 
 public class Rof{
   static String fenceF(int posts){
@@ -23,12 +36,12 @@ return output;
 }
 static String reverseR(String s){
   String output="";
-  if (s.length()==1){
-    System.out.println(output);
+  if (s.length()==0){
+    return output;
   }
   else{
-  output+=s.substring((s.length()-1),(s.length()));
-  reverseR(s.substring(0,(s.length()-1)));
+    output = s.substring((s.length()-1),(s.length()))+ reverseR(s.substring(0,(s.length()-1)));
+
 }
 return output;
 }
@@ -37,8 +50,8 @@ public static void main(String[] args){
   System.out.println(fenceF(0));
   System.out.println(fenceF(1));
   System.out.println(fenceF(2));
-  System.out.println(reverseF("stressed"));
-System.out.println(reverseR("stressed"));
+  System.out.println(reverseF("stressed dog"));
+  System.out.println(reverseR("stressed dog"));
 
 }
 
