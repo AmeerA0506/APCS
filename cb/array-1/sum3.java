4207c5056f685b7b60f78f2a9d36480a93b0c003
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-11-17 06:39:16 -0500

    Update sum3.java

--- cb/array-1/sum3.java
+++ cb/array-1/sum3.java
@@ -1,10 +1,16 @@
 //Given an array of ints length 3, return the sum of all the elements.
 
-
-public int sum3(int[] nums) {
+public class sum3 {
+  public static int sum3(int[] nums) {
   int sum=0;
   for(int i=0; i<nums.length;i+=1){
     sum+=nums[i];
   }
-  return sum;
-}
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(sums([1, 2, 3]); // 6
+    System.out.println(sums([5, 11, 2]); // 18
+    System.out.println(sums([7, 0, 0]); // 7            
+                       }
+                       }
