6474e6f53f9d83afca39a2406b2179001346fc48
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-11-17 06:39:43 -0500

    Update makePi.java

--- cb/array-1/makePi.java
+++ cb/array-1/makePi.java
@@ -1,7 +1,7 @@
 //Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 
 public class makePi{
-public int[] makePi() {
+public static int[] makePi() {
   int[] Pi=new int[3];
   Pi[0]=3;
   Pi[1]=1;
