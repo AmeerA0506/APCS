ce04067718ab15237840982344b39607d971a31c
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-12-05 23:20:20 -0500

    Update Rational.java

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -1,11 +1,10 @@
 /**
 (Do Not) Consume Arsenic:: Ameer Alnasser & Corina Chen with test cases from Thinker Gabriel Thompson
 APCS pd6
-HW41: Be Rational!
+HW42: Be Rational!
 2021-12-1
 time taken: .5hrs
-**/
-/**
+
 DISCO:
 protected seems like the best class for most variables, leaves very little to mess up in both directions
 toString should not have parameters
@@ -15,119 +14,149 @@ Visualizer is key, especially in mathematical situations in which there is a com
 
 should the error message be before or after the output?
 **/
-public class Rational{
-  protected int num;
-  protected int dem;
-  protected String output;
-  public Rational(){
-    num=0;
-    dem=1;
+public class Rational {
+  private int p; // Tis a denominator
+  private int q; // Tis a numberator
+
+  public Rational () {
+    //Creates a rational with 0/1
+    p = 0;
+    q = 1;
   }
-  public String toString(){
-    output= num + "/" + dem;
+
+  public Rational ( int a, int b ) {
+    //Creates a rational number of a/b unless b is equal to zero
+    p = a;
+    q = b;
+    if ( b == 0 ) {
+      System.out.println( "Denominator cannot be zero. It will take default value of one until you reconsider." );
+      q = 1;
+    }
+  }
+
+  //Returns a string representation of the rational number
+  public String toString() {
+    return p + "/" + q;
+  }
+
+  // Returns floating point approximation of the number using the most precise
+  // floating point primitive
+  public double floatValue() {
+    return (double) p / q; //type-casting the int into a double
+  }
+
+  // Returns the gcd of the numerator and denominator
+  // Imported from 26_gcd/Stats.java
+  public static int gcd( int a, int b) {
+    int min;
+    if (a <= b) {
+      min = a;
+    }
+    else { min = b;}
+
+    int currentNum = 1;
+    int guess = 1;
+
+    while ( currentNum < min ) {
+      if ( a % currentNum == 0 && b % currentNum ==0 ) {
+        guess = currentNum;
+      }
+      currentNum += 1;
+    }
+
+    return guess;
+  }
+
+  // Reducing with gcd
+  public void reduce() {
+    int low = gcd ( p, q );
+    p = p / low;
+    q = q / low;
+  }
+
+  // Compare two nums
+  public int compareTo ( Rational input ) {
+    double calling = this.floatValue();
+    double param = input.floatValue();
+    int output= (int) calling-param;
     return output;
   }
-  public Rational(int num2,int dem2){
-    this();
-    if(dem2 == 0){
-      System.out.println("undefined, default constructor running instead");
+
+  // Simply fraction multiplication
+  public void multiply( Rational input ) {
+    p *= input.p;
+    q *= input.q;
+  }
+
+  // Dividing fractions is equal to muliplying by the inverse of the divisor
+  public void divide ( Rational input ) {
+    if ( input.p != 0 ) {
+      p *= input.q;
+      q *= input.p;
     }
-    else{
-    num=num2;
-    dem=dem2;
-  }
-  }
-  public double floatValue(){
-    double g= (double) num/dem;
-    return g;
-  }
-  public void multiply(Rational factor){
-    num = factor.num*num;
-    dem = factor.dem*dem;
-  }
-  public void divide(Rational dvisor){
-    if (divisor.num!=0){
-    dem = dem*divisor.num;
+    else {
+      System.out.println( "Illegal. Division by 0 error.");
     }
-    else{
-    System.out.println("undefined, returning dividend in untouched manner");
-    }
-    num = num*factor.dem;
   }
 
-    public static int gcdER(int a, int b) {
-        if (a == b) {
-            return a;
-        }
-        if (a < b) {
-            return gcdER(a, b - a);
-        } else {
-            return gcdER(a - b, b);
-        }
-    }
-public void add(Rational addend){
-  int b=dem;
-  dem=dem*addend.dem;
-  addend.dem=dem;
-  num=num*addend.dem;
-  addend.num=b*addend.num;
-  num=addend.num+num;
-
-}
-public void subtract(Rational addend){
-  int b=dem;
-  dem=dem*addend.dem;
-  addend.dem=dem;
-  num=num*addend.dem;
-  addend.num=b*addend.num;
-  num=num-addend.num;
-}
-public void Simplify(){
-  int gcd=gcdER(num,dem);
-  num=num/gcd;
-  dem=dem/gcd;
-}
-public Rational compareTo(Rational comparer){
-int a=comparer.floatValue();
-int b=floatValue();
-return b-a;
-}
-  public static void main(String[] args){
-    System.out.println("\ntoString() and constructor test cases:");
-    Rational a = new Rational();
-    System.out.println(a + " ...should be \"0 / 1\"");
-    Rational b = new Rational(5, 2);
-    System.out.println(b + " ...should be \"5 / 2\"");
-    Rational c = new Rational(1, 0);
-    System.out.println(c + " ...should yield error");
-
-    System.out.println("\nfloatValue() test cases:");
-    Rational d = new Rational(6, 3);
-    System.out.println(d.floatValue() + " ...should be \"2.0\"");
-    Rational e = new Rational(5, 3);
-    System.out.println(e.floatValue() + " ...should be \"1.66666667\"");
-    Rational f = new Rational(-18, 9);
-    System.out.println(f.floatValue() + " ...should be \"-2.0\"");
-
-    System.out.println("\nmultiply() test cases:");
-    Rational g = new Rational(5, 4);
-    Rational h = new Rational(6, 10);
-    g.multiply(h);
-    System.out.println(g + " ...should be \"30 / 40\"");
-    Rational i = new Rational(5, 10);
-    Rational j = new Rational();
-    i.multiply(j);
-    System.out.println(i + " ...should be \"0 / 10\"");
-
-    System.out.println("\ndivide() test cases:");
-    Rational k = new Rational(10, 3);
-    Rational l = new Rational(6, 4);
-    k.divide(l);
-    System.out.println(k + " ...should be \"40 / 18\"");
-    // This next test cases checks for how the class reacts to divison by 0 in the divide() function
-    Rational m = new Rational(5, 5);
-    Rational n = new Rational();
-    m.divide(n);
-    System.out.println(m + " ...should yield an error\n");
+  // Takes 1 Rational object and adds onto current num
+  public void add ( Rational input ) {
+      int num1 = p * input.q;
+      int num2 = q * input.p;
+      p = num1 + num2;
+      q *= input.q;
   }
+
+  // Takes 1 Rational object and subtracts from current num
+  public void subtract ( Rational input ) {
+      int num1 = p * input.q;
+      int num2 = q * input.p;
+      p = num1 - num2;
+      q *= input.q;
+  }
+
+  public static void main ( String[] args ) {
+    Rational r = new Rational(2,3); // Stores rational number 2/3
+    Rational s = new Rational(1,2); // Stores rational number 1/2
+    Rational t = new Rational(4,18); //Stores the rational number 4/18
+
+    // Testing the toString
+    System.out.println( r.toString() ); // Should print out 2/3
+    System.out.println( s.toString() ); //Should print out 1/2
+
+    // Testing floatValue
+    System.out.println( r.floatValue() ); // Should print out 0.66666...
+    System.out.println ( s.floatValue() ); // Should print out 0.5
+
+    // Testing multiply
+    r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains 1/2
+    System.out.println( r.toString() ); // To check out what r is after running through multiply
+    System.out.println( s.toString() ); // To check that s remains 1/2
+
+    // Testing divide
+    r.divide(s);
+    System.out.println( r.toString() ); // Should return 4/6 because r is 2/6
+    System.out.println( s.toString() ); //Should still return 1/2
+
+    // Testing add
+    r.add(s);
+    System.out.println ( r.toString() ); // Should return 7/6
+    System.out.println( s.toString() ); // Should return 1/2
+
+    // Testing subtract
+    r.subtract(s);
+    System.out.println ( r.toString() ); // Should return 8/12
+    System.out.println( s.toString() ); // Should return 1/2
+
+    // Testing Reduce
+    t.reduce(); //Changes t to 2/9
+    System.out.println(t);
+
+    // Testing compareTo
+    Rational e = new Rational( 1, 2 );
+    System.out.println(r.compareTo(s)); // Should return 1
+    System.out.println(t.compareTo(s)); // Should return -1
+    System.out.println(e.compareTo(s)); // Should return 0
+  }
+
 }
