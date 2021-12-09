cc14ecfdd8367e0ddb55635a9db51d06a9a6cd7f
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-12-08 22:12:03 -0500

    Create ListInt.java

---
+++ 45_suparr/ListInt.java
@@ -0,0 +1,12 @@
+/**
+Always Alone::Ameer Alnasser
+APCS pd6
+HW45: Array of Titanium
+2021-12-8
+**/
+public Interface ListInt{
+  boolean add(int x){}
+  int get (int index){}
+  int set (int index, int x){}
+  int size(){}
+}
