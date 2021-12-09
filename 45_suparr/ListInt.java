54604781a698daaf295ba7262359a2e54dd27b5b
Ameer_Alnasser <aalnasser30@stuy.edu>
2021-12-08 22:15:43 -0500

    Update ListInt.java

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -4,9 +4,9 @@ APCS pd6
 HW45: Array of Titanium
 2021-12-8
 **/
-public Interface ListInt{
-  boolean add(int x){}
-  int get (int index){}
-  int set (int index, int x){}
-  int size(){}
+public interface ListInt{
+  boolean add(int x);
+  int get (int index);
+  int set (int index, int x);
+  int size();
 }
