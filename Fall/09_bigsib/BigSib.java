56874a8196951912037f7dad65ddbae3f453663e
Ameer_Alnasser <aalnasser30@stuy.edu>
2022-02-08 01:20:05 -0500

    Organization :)

---
+++ Fall/09_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/**Never Advise Children:: Ameer Alnasser-Turtle Boi, Corina Chen-Binktop, Nakib Abedin-Joker
+*Ameer Alnasser
+*APCS
+*HW09
+*2021-10-01
+*
+
+public class BigSib {
+
+
+	public static void greet(String input){
+		System.out.println("How is it going, fellow thinker"+input);
+}
+
+	public static void main(String[] args){
+		greet(" Ameer! How is the weather holding up?");
+		greet(" Jacob! Hope you woke up on the right side of bed :D");
+		greet(" Josiah! How was your transport to school today?");
+}
+
+}
