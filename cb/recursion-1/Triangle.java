da66432d8d8351b49a7d87ff3bdf32607a92cc60
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 20:58:02 -0500

    Update Triangle.java

--- cb/recursion-1/Triangle.java
+++ cb/recursion-1/Triangle.java
@@ -1,4 +1,5 @@
 public class Triangle {
+  //We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
 public int bunnyEars2(int bunnies) {
   if (bunnies == 0) {
     return 0;
