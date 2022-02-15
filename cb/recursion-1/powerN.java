e0787a363aad97640d06ce38b9422ff8fb6487f8
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 21:01:04 -0500

    Update powerN.java

--- cb/recursion-1/powerN.java
+++ cb/recursion-1/powerN.java
@@ -1,6 +1,6 @@
 public class powerN{
+//Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 public int powerN(int base, int n) {
-  //Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
   if(n==1){
     return base;
   }
