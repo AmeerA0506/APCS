c73307d96ceb61f05a0e6ebe092fb03b001db236
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 20:58:37 -0500

    Update array11.java

--- cb/recursion-1/array11.java
+++ cb/recursion-1/array11.java
@@ -1,4 +1,5 @@
 public class array11{
+  //Given an array of ints, compute recursively the number of times that the value 11 appears in the array. We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
 public int array11(int[] nums, int index) {
   if(index==nums.length){
     return 0;
