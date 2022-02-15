4fc0a34cc4c90e7978d3bfd6027ba5659b391edb
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 20:57:35 -0500

    Update Factorial.java

--- cb/recursion-1/Factorial.java
+++ cb/recursion-1/Factorial.java
@@ -1,4 +1,5 @@
 public class Factorial {
+  //Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
 public int factorial(int n) {
   if (n>1) {
     return n*factorial(n-1);
