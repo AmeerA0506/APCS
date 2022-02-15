418dfeeacd7f81ff5f40f09250c745d72bd55b74
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 21:00:48 -0500

    Update sumDigits.java

--- cb/recursion-1/sumDigits.java
+++ cb/recursion-1/sumDigits.java
@@ -1,4 +1,5 @@
 public class sumDigits{
+  //Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 public int sumDigits(int n) {
   if(n<10){
     return n;
