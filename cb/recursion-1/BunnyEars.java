79c38b4378e5d97351b577ae5c670069ff865f2e
Ameer Alnasser <aalnasser30@stuy.edu>
2022-02-14 20:56:04 -0500

    Update BunnyEars.java

--- cb/recursion-1/BunnyEars.java
+++ cb/recursion-1/BunnyEars.java
@@ -1,4 +1,5 @@
 public class BunnyEars {
+  //We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
 public int factorial(int n) {
   if (n>1) {
     return n*factorial(n-1);
