8e893f8e313b1ef4217fdaa059d6e52d779c9350
AmeerA0506 <aalnasser30@stuy.edu>
2022-03-02 07:05:51 -0500

    knight

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -1,8 +1,8 @@
-// Clyde Sinclair
-// APCS pd0
+// BA Mitzvah:: Ameer Alnasser, Ben Belotser, Michael Kamela
+// APCS pd6 
 // HW68 -- recursively probing for a closed cycle
 // 2022-02-28m
-// time spent:  hrs
+// time spent:.5  hrs
 
 /***
  * SKELETON
@@ -15,16 +15,18 @@
  * $ java KnightTour [N]
  *
  * ALGO
- *
- * DISCO
- *
+ * Given a starting position and board size, this method ultimately displays a completed knight tour (if possible) using numbers tp depict the possible steps being done. Due to the "unusualness" of the knights' moves, we had to look at every possible knight tour in order to find a completed one. This program does this by finding one "line" of possible knight moves, then stopping when it hits a "dead end" (no moves are available) After hitting a dead end, the knight returns to the last square without dead ends and goes through to find if you are able to complete it from a different point. It repeats this process until all 0s in the 2DArray are replaced by a number.
+ * DISC
+ *The purpose of the system.exit part is to exit the program in case its taking too long
  * QCC
- *
+ * Have we worked with using "arguments" while compiling and running a java file before? we used args[0] today and I felt like ive never seen it
+ * Whats the best way to measure time in terms of user/sys?
+*  It seems to have exponentially gone up as n got bigger
  * Mean execution times for boards of size n*n:
- * n=5   __s    across __ executions
- * n=6   __s    across __ executions
- * n=7   __s    across __ executions
- * n=8   __s    across __ executions
+ * n=5   .02s    across 5 executions
+ * n=6   .15s    across 5 executions
+ * n=7   1000s    across 1 executions
+ * n=8   1500s    across 1 executions
  *
  * POSIX PROTIP: to measure execution time from BASH, use time program:
  * $ time java KnightTour 5
@@ -64,14 +66,15 @@ public class KnightTour
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for random starting location, use lines below:
-    //int startX = (int) (8*Math.random());
-    //int startY = (int) (8*Math.random())
-    //tf.findTour( startX, startY, 1 );   // 1 or 0 ?
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //int startX = (int) (n*Math.random())+2;
+    //int startY = (int) (n*Math.random())+2;
+    //tf.findTour( startX, startY, 1 );   // 1 or 0 ?  
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // PUSHING FARTHER...
     // Systematically attempt to solve from every position on the board?
+    // We would need to do a for loop for y and a for loop for a startx and we were unable to do so with the time given
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   }//end main()
@@ -97,7 +100,13 @@ class TourFinder
     //SETUP BOARD --  0 for unvisited cell
     //               -1 for cell in moat
     //---------------------------------------------------------
-    ???
+    for (int i=0; i<_board.length; i++){
+	for(int j =0; j<_board.length; j++){
+		if(i<2 || j<2 || i> _board.length-3 || j>_board.length -3){
+			_board[i][j]=-1;
+		}
+}
+}
     //---------------------------------------------------------
 
   }//end constructor
@@ -153,21 +162,21 @@ class TourFinder
     if ( _solved ) System.exit(0);
 
     //primary base case: tour completed
-    if ( ??? ) {
-      ???
+    if ( moves==_sideLength*_sideLength+1 ) {
+      _solved=true;
       System.out.println( this ); //refresh screen
       return;
     }
     //other base case: stepped off board or onto visited cell
-    if ( ??? ) {
-      return;
+    if ( _board[x][y]!=0) {
+		return;
     }
     //otherwise, mark current location
     //and recursively generate tour possibilities from current pos
     else {
 
       //mark current cell with current move number
-      _board[x][y] = ???
+      _board[x][y] =moves;
 
       System.out.println( this ); //refresh screen
 
@@ -182,11 +191,18 @@ class TourFinder
        *     g . . . b
        *     . h . a .
       ******************************************/
-      ???
-
+      findTour(x-2,y+1,moves+1);
+	findTour(x-1,y+2,moves+1);
+	findTour(x+1,y+2,moves+1);
+	findTour(x+2,y+1,moves+1);
+	findTour(x+2,y-1,moves+1);
+	findTour(x+1,y-2,moves+1);
+	findTour(x-1,y-2,moves+1);
+	findTour(x-2,y-1,moves+1);
+	//given how similar they all look, i thinkt hat it is possible to do a for loop in a for loop from x-2 to x+2 with an embedded one with y-2-> y+2 excluding y to do this but thats no fun
       //If made it this far, path did not lead to tour, so back up...
       // (Overwrite number at this cell with a 0.)
-        ???
+        _board[x][y]=0;
 
       System.out.println( this ); //refresh screen
     }
