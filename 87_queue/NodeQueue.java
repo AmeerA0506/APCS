de3fdd797e87d9318b0c7888023f1520ca6e4e4d
Ameer_Alnasser <aalnasser30@stuy.edu>
2022-04-02 23:07:38 -0400

    for fang :(

--- 87_queue/NodeQueue.java
+++ 87_queue/NodeQueue.java
@@ -1,25 +1,77 @@
-public class NodeQueue<QUASAR> implements Queue{
-  public QUASAR dequeue(){
-    remove()
+public class NodeQueue<QUASAR> implements Queue<QUASAR>{
+
+  LLNode<QUASAR> line;
+
+  public NodeQueue(){
+    line=new LLNode<QUASAR>(null,null);
   }
 
+  public NodeQueue(QUASAR x){
+    line=new LLNode<QUASAR>(x,null);
+  }
+//
+  public QUASAR dequeue(){
+    LLNode<QUASAR> tmp=line; //phosia
+    QUASAR output=peekFront();//phosia
+    if(isEmpty()){
+    return null;
+  }
+    if(tmp.getNext()==null){ //phosia
+      tmp=new LLNode(null,null);//
+      return output;
+    }
+    while(tmp.getNext().getNext()!=null){
+      tmp=tmp.getNext();
+    }
+    tmp.setNext(null);
+    return output;
+  }
+// v,w,x,y,z
   //means of adding an element to collection:
   //Enqueue an element onto the back of this queue.
-  public void enqueue( QUASAR x );
-
+  public void enqueue( QUASAR x ){
+    if(isEmpty()){
+      line=new LLNode<QUASAR>(x, null);
+    }
+    line=new LLNode<QUASAR>(x, line);
+  }
   //Returns true if this queue is empty, otherwise returns false.
-  public boolean isEmpty();
+  public boolean isEmpty(){
+    return (line.getCargo()==null);
+  }
 
   //Returns the first element of the queue without dequeuing it.
-  public QUASAR peekFront();
-  public class LLNode
+  public QUASAR peekFront(){
+    LLNode<QUASAR> tmp=line; //phosia
+    if(isEmpty()){
+      return null;
+    }
+    while(tmp.getNext()!=null){
+      tmp=tmp.getNext();
+    }
+    QUASAR output=tmp.getCargo();
+    return output;
+    }
+
+  public String toString(){
+    String output="";
+    LLNode<QUASAR> tmp=line;
+    while(tmp.getNext()!=null){
+      output+=tmp.getCargo()+"<-";
+      tmp=tmp.getNext();
+    }
+    output+=tmp.getCargo();
+    return output;
+  }
+
+  public class LLNode<QUASAR>
   {
     //instance vars
-    private String _cargo;
-    private LLNode _nextNode;
+    private QUASAR _cargo;
+    private LLNode<QUASAR> _nextNode;
 
     // constructor
-    public LLNode( String value, LLNode next )
+    public LLNode( QUASAR value, LLNode<QUASAR> next )
     {
       _cargo = value;
       _nextNode = next;
@@ -27,12 +79,12 @@ public class NodeQueue<QUASAR> implements Queue{
 
 
     //--------------v  ACCESSORS  v--------------
-    public String getCargo()
+    public QUASAR getCargo()
     {
       return _cargo;
     }
 
-    public LLNode getNext()
+    public LLNode<QUASAR> getNext()
     {
       return _nextNode;
     }
@@ -40,16 +92,16 @@ public class NodeQueue<QUASAR> implements Queue{
 
 
     //--------------v  MUTATORS  v--------------
-    public String setCargo( String newCargo )
+    public QUASAR setCargo( QUASAR newCargo )
     {
-      String foo = getCargo();
+      QUASAR foo = getCargo();
       _cargo = newCargo;
       return foo;
     }
 
-    public LLNode setNext( LLNode newNext )
+    public LLNode<QUASAR> setNext( LLNode newNext )
     {
-      LLNode foo = getNext();
+      LLNode<QUASAR> foo = getNext();
       _nextNode = newNext;
       return foo;
     }
@@ -59,45 +111,22 @@ public class NodeQueue<QUASAR> implements Queue{
     // override inherited toString
     public String toString()
     {
-      return _cargo;
+      return (String)_cargo;
     }
-
-
-    //main method for testing
-    public static void main( String[] args )
-    {
-
-      //Below is an exercise in creating a linked list...
-
-      //Create a node
-      LLNode first = new LLNode( "cat", null );
-
-      //Create a new node after the first
-      first.setNext( new LLNode( "dog", null ) );
-
-      //Create a third node after the second
-      first.getNext().setNext( new LLNode( "cow", null ) );
-
-      /* A naive list traversal, has side effects.... ??
-         while( first != null ) {
-           System.out.println( first );
-           first = first.getNext();
-         }
-      */
-
-      //Q: when head ptr moves to next node in list, what happens to the node it just left?
-      //A: garbage collector reclaims that memory
-
-      //  so, better: (w/o moving first)
-      /*
-        LLNode temp = first;
-        while( temp != null ) {
-          System.out.println( temp );
-          temp = temp.getNext();
-        }
-      */
-
-    }//end main
-
   }//end class LLNode
+
+  public static void main(String[] args){
+    NodeQueue fangMeanie= new NodeQueue("3");
+    System.out.println(fangMeanie);
+    fangMeanie.enqueue("Ameer");
+    System.out.println(fangMeanie);
+    fangMeanie.enqueue("Phosia");
+    System.out.println(fangMeanie);
+    fangMeanie.dequeue();
+    System.out.println(fangMeanie);
+    fangMeanie.dequeue();
+    System.out.println(fangMeanie);
+    fangMeanie.dequeue();
+    System.out.println(fangMeanie);
+  }
 }
