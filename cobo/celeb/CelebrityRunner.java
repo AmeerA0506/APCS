dc793f437029b027f043c184b9812852fa94d66b
Ameer_Alnasser <aalnasser30@stuy.edu>
2022-04-26 18:52:29 -0400

    :(

---
+++ cobo/celeb/CelebrityRunner.java
@@ -0,0 +1,31 @@
+// National Security Agency:: Nora Miller, Sophia Eiden, Ameer Alnasser
+// APCS pd 6
+// L09: Some Folks Call It A Charades
+// 2022-04-26
+
+/**
+ * Starts the Celebrity Game application
+ * @author cody.henrichsen
+ *
+ */
+ import java.util.Scanner;
+public class CelebrityRunner
+{
+	/**
+	 * The entry point of the Java application.
+	 * @param args Unused parameter as it is a GUI based program.
+	 */
+	public static void main(String [] args)
+	{
+		CelebrityGame a = new CelebrityGame();
+    System.out.println(a.prepareGame());
+		//instantiate a Scanner with STDIN as its bytestream
+		//Scanner sc = new Scanner( System.in );
+
+    // prompt the user to
+
+    //a.add()
+
+
+	}
+}
